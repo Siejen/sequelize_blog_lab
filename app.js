4b71c1997014e27c4f43c07aa50012c930236af3
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-07-31 13:48:52 -0700

    Adding the sign up page to the app

--- app.js
+++ app.js
@@ -1,10 +1,115 @@
 var express = require("express"),
-	bodyParser = require('body-parser'),
-  	db = require("./models/index.js"),
-  	app = express();
+  bodyParser = require("body-parser"),
+  passport = require("passport"),
+  passportLocal = require("passport-local"),
+  cookieParser = require("cookie-parser"),
+  cookieSession = require("cookie-session"),
+  flash = require("connect-flash"),
+  app = express(),
+  db = require("./models/index");
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded());
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}) ); 
+
+//////
+
+app.use(cookieSession({
+  secret: 'thisismysecretkey',
+  name: 'cookie created by Siejen',
+  //maxage is in milliseconds
+  maxage: 360000
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// prepare our serialize functions
+passport.serializeUser(function(author,done){         // Serialize and deserialize will run independently on its own
+  done(null, author.id);
+});
+
+// a tiny query to see if our author/id is still in the database
+passport.deserializeUser(function(id,done){
+  db.author.find({
+    where: {
+      id: id
+    }
+  }).done(function(error, author){
+    done(error, author);
+  });
+});
+
+app.get('/', function(req,res){
+  // check if the author is logged in
+  if(!req.author) {
+    res.render("blog");    
+  }
+  else {
+    res.redirect('/newpost');
+  }
+});
+
+app.get('/signup', function(req,res){
+  if(!req.author) {
+    res.render("signup",{username: ""});
+  }
+  else{
+    res.redirect('/home');
+  }
+});
+
+app.get('/login', function(req,res){
+  if(!req.author) {
+    res.render("login");
+  }
+  else {
+    res.redirect('/home');
+  }
+});
+
+app.get('/home', function(req,res){
+  res.render("home", {
+    isAuthenticated: req.isAuthenticated(),     // isAuthenticated is a passport function
+    author: req.author 
+  });
+});
+// on submit, create a new authors using form values
+app.post('/create', function(req,res){  
+    db.author.create({content: req.body.content})
+    .success(function(author) {
+
+  db.author.createNewAuthor(req.body.username, req.body.password, 
+  function(err){
+    res.render("signup", {message: err.message, username: req.body.username});
+  }, 
+  function(success){
+    res.render("index", {message: success.message});
+  });
+});
+});
+
+// authenticate authors when logging in
+// app.post('/login', passport.authenticate('local', {
+//  successRedirect: '/home',
+//  failureRedirect: '/foo',
+//  failureFlash: true,   
+// }));
+
+app.post('/login', passport.authenticate('local', { 
+  successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+    })
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+//////
 
 app.get("/blog", function(req, res){
   db.post.findAll().success(function(posts){
